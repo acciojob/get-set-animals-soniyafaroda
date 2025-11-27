c0b2aa9aeb8e89ac01daa1e03cd8694f89603f4d
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-27 16:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-//complete this code
-class Animal {}
+// --------------------------
+// Animal Class
+// --------------------------
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method: makeSound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
+// --------------------------
+// Cat Class (inherits Animal)
+// --------------------------
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// --------------------------
+// Dog Class (inherits Animal)
+// --------------------------
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// --------------------------
 // Do not change the code below this line
+// --------------------------
 window.Animal = Animal;
-window.Dog = Dog;
 window.Cat = Cat;
+window.Dog = Dog;
